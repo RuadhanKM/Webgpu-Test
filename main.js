90b2efd9ea10db7b7fb3453b46304dc419dc4962
9281239 <9281239@students.k12.dc.us>
2023-08-25 11:18:56 -0400

    added basic terrain

--- main.js
+++ main.js
@@ -37,12 +37,37 @@ canvasUi.addEventListener("mousedown", async e => {
 });
 
 const blockSize = 1
-const renderDistance = 10
+const renderDistance = 6
 const chunkSize = 24
+const seed = 21
 
 const chunks = {}
 let vArrays = {}
 let visableBlocks = {}
+
+const p = new Array(512);
+const permutation = [
+    151, 160, 137, 91, 90, 15, 131, 13, 201, 95, 96, 53, 194, 233, 7, 225, 140,
+    36, 103, 30, 69, 142, 8, 99, 37, 240, 21, 10, 23, 190, 6, 148, 247, 120, 234,
+    75, 0, 26, 197, 62, 94, 252, 219, 203, 117, 35, 11, 32, 57, 177, 33, 88, 237,
+    149, 56, 87, 174, 20, 125, 136, 171, 168, 68, 175, 74, 165, 71, 134, 139, 48,
+    27, 166, 77, 146, 158, 231, 83, 111, 229, 122, 60, 211, 133, 230, 220, 105,
+    92, 41, 55, 46, 245, 40, 244, 102, 143, 54, 65, 25, 63, 161, 1, 216, 80, 73,
+    209, 76, 132, 187, 208, 89, 18, 169, 200, 196, 135, 130, 116, 188, 159, 86,
+    164, 100, 109, 198, 173, 186, 3, 64, 52, 217, 226, 250, 124, 123, 5, 202, 38,
+    147, 118, 126, 255, 82, 85, 212, 207, 206, 59, 227, 47, 16, 58, 17, 182, 189,
+    28, 42, 223, 183, 170, 213, 119, 248, 152, 2, 44, 154, 163, 70, 221, 153, 101,
+    155, 167, 43, 172, 9, 129, 22, 39, 253, 19, 98, 108, 110, 79, 113, 224, 232,
+    178, 185, 112, 104, 218, 246, 97, 228, 251, 34, 242, 193, 238, 210, 144, 12,
+    191, 179, 162, 241, 81, 51, 145, 235, 249, 14, 239, 107, 49, 192, 214, 31,
+    181, 199, 106, 157, 184, 84, 204, 176, 115, 121, 50, 45, 127, 4, 150, 254,
+    138, 236, 205, 93, 222, 114, 67, 29, 24, 72, 243, 141, 128, 195, 78, 66, 215,
+    61, 156, 180,
+];
+
+for (let i = 0; i < 256; i++) {
+    p[256 + i] = p[i] = permutation[i] & seed;
+}
 
 function createBlockVertices([x, y, z], id, textureSize) {
     id--
@@ -431,20 +456,145 @@ function chunkInRenderDis(chunkX, chunkY, chunkZ) {
     return Math.sqrt((chunkX-playerChunk[0])**2+(chunkY-playerChunk[1])**2+(chunkZ-playerChunk[2])**2) < renderDistance/2
 }
 
+function fade(t) {
+    return t * t * t * (t * (t * 6 - 15) + 10);
+};
+
+function lerp(t, a, b) {
+    return a + t * (b - a);
+}
+
+function grad(hash, x, y, z) {
+    const h = hash & 15
+    const u = h < 8 ? x : y
+    const v = h < 4 ? y : h == 12 || h == 14 ? x : z
+    return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v)
+}
+
+function perlinNoise(x, y, z) {
+    const X = Math.floor(x) & 255
+    const Y = Math.floor(y) & 255
+    const Z = Math.floor(z) & 255
+    
+    x -= Math.floor(x);
+    y -= Math.floor(y);
+    z -= Math.floor(z);
+    const u = fade(x)
+    const v = fade(y)
+    const w = fade(z)
+    const A = p[X] + Y
+    const AA = p[A] + Z
+    const AB = p[A + 1] + Z
+    const B = p[X + 1] + Y
+    const BA = p[B] + Z
+    const BB = p[B + 1] + Z
+
+    return lerp(
+        w,
+        lerp(
+            v,
+            lerp(u, grad(p[AA], x, y, z), grad(p[BA], x - 1, y, z)),
+            lerp(u, grad(p[AB], x, y - 1, z), grad(p[BB], x - 1, y - 1, z))
+        ),
+        lerp(
+            v,
+            lerp(u, grad(p[AA + 1], x, y, z - 1), grad(p[BA + 1], x - 1, y, z - 1)),
+            lerp(
+                u,
+                grad(p[AB + 1], x, y - 1, z - 1),
+                grad(p[BB + 1], x - 1, y - 1, z - 1)
+            )
+        )
+    );
+}
+
+function mulberry32(a) {
+    var t = a + 0x6D2B79F5;
+    t = Math.imul(t ^ t >>> 15, t | 1);
+    t ^= t + Math.imul(t ^ t >>> 7, t | 61);
+    return ((t ^ t >>> 14) >>> 0) / 4294967296;
+}
+
+let cellCache = {}
+function cellNoise(x, y, z) {
+    let gridSize = 10
+    let minDis = Infinity
+    let bc = vec3.div([x, y, z], gridSize).map(Math.round)
+    if (cellCache[bc]) {
+        for (const p of cellCache[bc]) {
+            let dis = vec3.dis(p, [x, y, z])
+            if (dis < minDis) minDis = dis
+        }
+    } else {
+        cellCache[bc] = []
+        for (let ox=-1; ox<=1; ox++) {
+            for (let oy=-1; oy<=1; oy++) {
+                for (let oz=-1; oz<=1; oz++) {
+                    let chunk = vec3.add(bc, [ox, oy, oz])
+                    let r = mulberry32(chunk[0])*1000+mulberry32([chunk[1]])*1000+mulberry32([chunk[2]])
+
+                    let rx = mulberry32(r*1000)
+                    let ry = mulberry32(rx*1000)
+                    let rz = mulberry32(ry*1000)
+
+                    let p = [(chunk[0]+rx)*gridSize, (chunk[1]+ry)*gridSize, (chunk[2]+rz)*gridSize]
+
+                    cellCache[bc].push(p)
+
+                    let dis = vec3.dis(p, [x, y, z])
+                    if (dis < minDis) minDis = dis
+                }
+            }
+        }
+    }
+    return minDis
+}
+
+
+let airLayer = new ArrayBuffer(chunkSize)
+let airChunk = new ArrayBuffer(chunkSize)
+
+for (let y=0; y<chunkSize; y++) {
+    let layer = new ArrayBuffer(chunkSize)
+    let layerRow = new ArrayBuffer(chunkSize)
+    for (let x=0; x<chunkSize; x++) {
+        let chunkRow = new ArrayBuffer(chunkSize)
+        layerRow[x] = 0
+        for (let z=0; z<chunkSize; z++) {
+            chunkRow[z] = 0
+        }
+        layer[x] = chunkRow
+    }
+    airChunk[y] = layer
+    airLayer[y] = layerRow
+}
+
 function createChunk(chunkX, chunkY, chunkZ) {
     if (getChunk(chunkX, chunkY, chunkZ)) return
+    if (chunkY > Math.ceil(124/chunkSize)) {
+        return airChunk.slice()
+    }
     let blockArray = new ArrayBuffer(chunkSize)
 
-    for (let y=0; y<chunkSize; y++) {
+    for (let relY=0; relY<chunkSize; relY++) {
         let layer = new ArrayBuffer(chunkSize)
-        for (let x=0; x<chunkSize; x++) {
-            let row = new ArrayBuffer(chunkSize)
-            for (let z=0; z<chunkSize; z++) {
-                row[z] = y+chunkY*chunkSize < 0 ? 2 : y+chunkY*chunkSize == 0 ? 1 : 0
-            }
-            layer[x] = row
+        let y = relY+chunkY*chunkSize
+        if (y > 124) {
+            blockArray[relY] = airLayer.slice()
         }
-        blockArray[y] = layer
+        for (let relX=0; relX<chunkSize; relX++) {
+            let row = new ArrayBuffer(chunkSize)
+            let x = relX+chunkX*chunkSize
+            for (let relZ=0; relZ<chunkSize; relZ++) {
+                let z = relZ+chunkZ*chunkSize
+                
+                let height = Math.round(perlinNoise(x/250, 0, z/250) * 80 * (perlinNoise(x/300, 0, z/300) + 0.3)) + Math.round(perlinNoise(x/30, 0, z/30) * 20 * perlinNoise(x/300, 0, z/300))
+
+                row[relZ] = (cellNoise(x, y, z) < 7.65) ? (y < height ? 2 : y == height ? 1 : 0) : 0
+            }
+            layer[relX] = row
+        }
+        blockArray[relY] = layer
     }
 
     chunks[getChunkNameFromPos(chunkX, chunkY, chunkZ)] = blockArray
